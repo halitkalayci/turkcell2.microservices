0925fb55a730fb65ffaa182ca66d7b5404b4e8eb
halitklyc <halitenes.kalayci@gmail.com>
2025-02-07 20:32:25 +0300

    discovery server

--- cart-service/src/main/java/com/turkcell/cartservice/controllers/CartController.java
+++ cart-service/src/main/java/com/turkcell/cartservice/controllers/CartController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CartController {
     @GetMapping
     public String get() {
+        System.out.println("get called");
         return "Cart Service";
     }
 }
