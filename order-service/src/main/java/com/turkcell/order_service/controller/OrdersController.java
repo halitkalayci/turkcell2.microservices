e7ab38fcfec39326a31dc49e4c16a5b343423d16
halitklyc <halitenes.kalayci@gmail.com>
2025-02-08 10:37:44 +0300

    rest-client ile sync iletisim

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -1,14 +1,29 @@
 package com.turkcell.order_service.controller;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
+    @LoadBalanced
+    private final RestClient restClient;
+
+    public OrdersController(RestClient.Builder restClientBuilder) {
+        restClient = restClientBuilder.baseUrl("http://localhost:8084").build();
+    }
+
     @GetMapping
     public String get() {
+        // istek at
+        // anında cevap al (cevabı bekle)
+        // cevabı işle (sync)
+        String response = restClient.get().uri("/api/v1/products").retrieve().body(String.class);
+        System.out.println("Product serviceden gelen cevap: " + response);
+        // mesajı gönder ve işleme devam et (async)
         return "Order Service";
     }
 }
