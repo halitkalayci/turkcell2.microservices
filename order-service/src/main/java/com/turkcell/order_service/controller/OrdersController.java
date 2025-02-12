8184c77321b93f5542e36c18302ba190e6dddef6
halitkalayci <halit.kalayci@ext.etiya.com>
2025-02-12 19:29:24 +0300

    .

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -1,6 +1,8 @@
 package com.turkcell.order_service.controller;
 
 import com.turkcell.order_service.client.ProductClient;
+import com.turkcell.order_service.entity.Order;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
     private final ProductClient productClient;
-
-    public OrdersController(ProductClient productClient) {
+    private final StreamBridge streamBridge;
+    public OrdersController(ProductClient productClient, StreamBridge streamBridge) {
         this.productClient = productClient;
+        this.streamBridge = streamBridge;
     }
 
     @GetMapping
     public String get() {
-        String response = productClient.get();
-        System.out.println(response);
+        // Syncİletişim
+        //String response = productClient.get();
+        //System.out.println(response);
+
+        // TODO: Kafkadan common bir classı gönder.
+        Order order = new Order();
+        order.setId("abc123");
+        //
+
+        streamBridge.send("orderCreatedFunction-out-0", "Mesaj123");
         return "Order Service";
     }
 }
---
+++ order-service/src/main/java/com/turkcell/order_service/entity/Order.java
@@ -0,0 +1,13 @@
+package com.turkcell.order_service.entity;
+
+public class Order {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
