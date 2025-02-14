d8d35eb4c2c93c6043e425f780d4e7a2e30155cc
halitkalayci <halit.kalayci@ext.etiya.com>
2025-02-14 19:54:40 +0300

    latest pom.xml

---
+++ turkcell-t2/target/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"io.github.halitkalayci","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"io.github.halitkalayci","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
